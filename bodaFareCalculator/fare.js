297ab87dddd033fbe8f122ac8bfe8a4322c6910c
George-alt5 <georgekihoro6@gmail.com>
2025-06-12 21:05:40 +0300

    bodaFareCalculator/fare.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -4,9 +4,3 @@ function calculateBodaFare(distanceInKm){
     const DistanceFare = distanceInKm * chargePerKm
     const total = BaseFare + DistanceFare
 }
-
-console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
-console.log(`Ukikalia Pikipiki: KES ${BaseFare}`);
-console.log(`Mpaka Uko: KES ${DistanceFare}`);
-console.log(`Total: KES ${total}`);
-console.log('Panda Pikipiki!')
